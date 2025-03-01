800bf933008c4ab623d50984d57038ca82d31eb9
rajputshashank003 <hariomshashank@gmail.com>
2025-03-01 23:18:04 +0530

    add new Auth button component

--- client/src/components/AppBar.tsx
+++ client/src/components/AppBar.tsx
@@ -2,6 +2,20 @@ import { useNavigate } from 'react-router-dom'
 import AppBarLogo from './AppBarLogo';
 import { useVerifyMe } from '../hooks/useVerifyMe';
 import Ratsi from './Ratsi';
+import { ReactNode } from 'react';
+
+interface AuthButtonProps {
+    children : ReactNode ;
+    handleClick : () => void;
+}
+
+const AuthButton = ({children , handleClick} : AuthButtonProps) => {
+    return (
+        <div onClick={handleClick} className="absolute right-2 top-1/2 -translate-y-1/2 hover:opacity-[0.7] duration-200 cursor-pointer duration-200 text-[1.4rem] flex justify-center items-center sm:text-[1.8rem] px-4 rounded-xl font-semibold bg-transparent border border-zinc-200 text-zinc-200 font-sans">
+            {children}
+        </div>
+    )
+}
 
 const AppBar = () => {
     const navigate = useNavigate();
@@ -12,18 +26,18 @@ const AppBar = () => {
             <div className="absolute top-1/2 hover:opacity-[0.8] duration-200 -translate-y-1/2 left-1">
                 <AppBarLogo/>
             </div>
-            <div onClick={() => navigate("/products")} className='absolute z-[99999] max-sm:hidden right-40 top-1/2 -translate-y-1/2'>
+            <div onClick={() => navigate("/products")} className='absolute max-sm:hidden right-40 top-1/2 -translate-y-1/2'>
                 <Ratsi/>
             </div>
             {
                 isAuthenticated ?
-                <div onClick={logout} className="absolute z-[999999] right-2 top-1/2 -translate-y-1/2 hover:opacity-[0.7] duration-200 cursor-pointer duration-200 text-[1.5rem] flex justify-center items-center sm:text-[1.8rem] px-2 rounded-xl font-bold bg-zinc-200 font-sans">
+                <AuthButton handleClick={logout}>
                     Logout
-                </div>
+                </AuthButton>
                 :
-                <div onClick={() => navigate("/signin") } className="absolute z-[999999] right-2 top-1/2 -translate-y-1/2 hover:opacity-[0.7] duration-200 cursor-pointer duration-200 text-[1.4rem] flex justify-center items-center sm:text-[1.8rem] px-4 rounded-xl font-semibold bg-transparent border border-zinc-200 text-zinc-200 font-sans">
+                <AuthButton handleClick={() => navigate("/signin")}>
                     Login
-                </div>
+                </AuthButton>
             }
         </div>
     )
